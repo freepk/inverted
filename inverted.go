83820174eaf6d7222101435591ca206840f87e76
freepk <freepk@mail.ru>
2018-03-29 20:25:56 +0300

    Update inverted.go

--- inverted.go
+++ inverted.go
@@ -1,110 +1,81 @@
 package inverted
 
 import (
+	"sync"
+
 	"github.com/freepk/radix"
 )
 
 const (
-	maxTailSize = 4096
+	smallIcebergSize = 1024
 )
 
-type sortedArray struct {
+type iceberg struct {
+	top  []int
 	body []int
-	tail []int
 }
 
-func newSortedArray() *sortedArray {
-	return &sortedArray{
-		body: make([]int, 0, maxTailSize),
-		tail: make([]int, 0, maxTailSize)}
+func newIceberg() *iceberg {
+	body := make([]int, 0, smallIcebergSize)
+	return &iceberg{
+		top:  body,
+		body: body}
 }
 
-func (a *sortedArray) union() {
-	bs := len(a.body)
-	ts := len(a.tail)
-	if ts == 0 {
+func (i *iceberg) visible() bool {
+	return len(i.top) == len(i.body)
+}
+
+// TODO: make sorting in backgound with separate schedule logic
+func (i *iceberg) show() {
+	if i.visible() {
 		return
 	}
-	buff := make([]int, (bs + ts))
-	radix.Ints(a.tail, buff, ts)
-	buff = buff[:0]
-	prev, curr := 0, 0
-	i, j := 0, 0
-	for i < bs && j < ts {
-		switch {
-		case a.body[i] < a.tail[j]:
-			curr = a.body[i]
-			i++
-		case a.body[i] > a.tail[j]:
-			curr = a.tail[j]
-			j++
-		default:
-			curr = a.body[i]
-			i++
-			j++
-		}
-		if prev != curr {
-			prev = curr
-			buff = append(buff, curr)
-		}
-	}
-	for i < bs {
-		curr = a.body[i]
-		i++
-		if prev != curr {
-			prev = curr
-			buff = append(buff, curr)
-		}
-	}
-	for j < ts {
-		curr = a.tail[j]
-		j++
-		if prev != curr {
-			prev = curr
-			buff = append(buff, curr)
-		}
-	}
-	a.body = buff
-	a.tail = a.tail[:0]
+	size := len(i.body)
+	buff := make([]int, size)
+	radix.Ints(i.body, buff, size)
+	i.top = i.body
 }
 
-func (a *sortedArray) append(value int) {
-	if len(a.tail) == maxTailSize {
-		a.union()
-	}
-	a.tail = append(a.tail, value)
+func (i *iceberg) append(value int) {
+	i.body = append(i.body, value)
 }
 
-func (a *sortedArray) items() []int {
-	a.union()
-	return a.body
+func (i *iceberg) items() []int {
+	i.show()
+	return i.top
 }
 
 type Index struct {
-	tags    map[int]*sortedArray
-	trashed int
+	tokens *sync.Map
 }
 
 func NewIndex() *Index {
-	return &Index{
-		tags: make(map[int]*sortedArray)}
+	return &Index{tokens: &sync.Map{}}
 }
 
-func (x *Index) Append(key, tag int) {
-	if x.tags[tag] == nil {
-		x.tags[tag] = newSortedArray()
+func (x *Index) Append(key, token int) {
+	list, ok := x.tokens.Load(token)
+	if !ok {
+		list = newIceberg()
+		x.tokens.Store(token, list)
 	}
-	x.tags[tag].append(key)
+	list.(*iceberg).append(key)
 }
 
-func (x *Index) Tags() []int {
-	tags := make([]int, 0, len(x.tags))
-	for tag := range x.tags {
-		tags = append(tags, tag)
+func (x *Index) Tokens() []int {
+	tokens := make([]int, 0)
+	x.tokens.Range(func(k, v interface{}) bool {
+		tokens = append(tokens, k.(int))
+		return true
+	})
+	return tokens
+}
+
+func (x *Index) ByToken(token int) []int {
+	list, ok := x.tokens.Load(token)
+	if !ok {
+		return []int{}
 	}
-	return tags
-}
-
-func (x *Index) ByTag(tag int) []int {
-	return x.tags[tag].items()
+	return list.(*iceberg).items()
 }
