b90f5dd7adacc6dc39032a73e5d526f5c45ab6ef
freepk <freepk@mail.ru>
2018-04-02 17:54:50 +0300

    Update iterator.go

--- iterator.go
+++ iterator.go
@@ -1,28 +1,68 @@
 package inverted
 
-/*
 type Iterator interface {
-	func Next() int, bool
-	func Reset()
+	Next() (int, bool)
 }
 
-Plain(int) Iterator
-Union([]Iterator) Iterator
-Inter([]Iterarot) Iterator
+type ArrayIterator struct {
+	array  []int
+	offset int
+}
 
-// (1 || 2 || 3 || 4) && (5 || 6 || 7 || 8)
-
-it := Inter([]Iterator{
-	Union( []Iterator{Plain(1), Plain(1), Plain(1), Plain(1)} ),
-	Union( []Iterator{Plain(5), Plain(6), Plain(7), Plain(8)} ),
-})
-
-it.Reset()
-for {
-	key, ok := it.Next()
-	if !ok {
-		break
+func (it *ArrayIterator) Next() (int, bool) {
+	if it.offset < len(it.array) {
+		offset := it.offset
+		it.offset++
+		return it.array[offset], true
 	}
-	...
+	return 0, false
 }
-*/
+
+type IntersectIterator struct {
+	array []Iterator
+}
+
+func (it *IntersectIterator) Next() (int, bool) {
+	size := len(it.array)
+	if size == 0 {
+		return 0, false
+	}
+	if size == 1 {
+		return it.array[0].Next()
+	}
+	ok := false
+	advice := 0
+	values := make([]int, size)
+	for i := 0; i < size; i++ {
+		values[i], ok = it.array[i].Next()
+		if !ok {
+			return 0, false
+		}
+		if i == 0 || advice < values[i] {
+			advice = values[i]
+		}
+	}
+	for {
+		for i := 0; i < size; i++ {
+			if values[i] == advice {
+				continue
+			}
+			for values[i] < advice {
+				values[i], ok = it.array[i].Next()
+				if !ok {
+					return 0, false
+				}
+			}
+		}
+		equals := 0
+		for i := 0; i < size; i++ {
+			if values[i] == advice {
+				equals++
+			}
+		}
+		if equals == size {
+			return advice, true
+		}
+	}
+	return 0, false
+}
