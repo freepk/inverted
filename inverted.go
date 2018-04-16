feea5a85714fbb11b64d18236599d8a6e829e18a
FreePK <freepk@mail.ru>
2018-04-16 18:22:36 +0300

    refactor

--- inverted.go
+++ inverted.go
@@ -23,39 +23,39 @@ func (i *Index) Append(item int, tokens []int) {
 	i.updItemTokens[item] = tokens
 }
 
-func (i *Index) Item(item int) []int {
-	return i.itemTokens[item]
+func (i *Index) Item(key int) []int {
+	return i.itemTokens[key]
 }
 
 func (i *Index) Items(token int) []int {
 	return i.tokenItems[token]
 }
 
-func (i *Index) update() {
-	var inserted, deleted, itemTokens, tokenItems map[int][]int
-	var item, token, u, c int
-	var update, current []int
-	var ok bool
+func (i *Index) Update() {
+	i.update()
+}
 
-	deleted = make(map[int][]int)
-	inserted = make(map[int][]int)
-	itemTokens, i.updItemTokens = i.updItemTokens, make(map[int][]int)
-	for item, update = range itemTokens {
+func (i *Index) update() {
+	deleted := make(map[int][]int)
+	inserted := make(map[int][]int)
+	itemTokens := i.updItemTokens
+	i.updItemTokens = make(map[int][]int)
+	for key, update := range itemTokens {
 		sort.Ints(update)
 		update = arrays.Distinct(update)
-		itemTokens[item] = update
-		current = i.itemTokens[item]
-		u = 0
-		c = 0
+		itemTokens[key] = update
+		current := i.itemTokens[key]
+		u := 0
+		c := 0
 		for (u < len(update)) && (c < len(current)) {
 			switch {
 			case current[c] < update[u]:
-				token = current[c]
-				deleted[token] = append(deleted[token], item)
+				token := current[c]
+				deleted[token] = append(deleted[token], key)
 				c++
 			case current[c] > update[u]:
-				token = update[u]
-				inserted[token] = append(inserted[token], item)
+				token := update[u]
+				inserted[token] = append(inserted[token], key)
 				u++
 			default:
 				c++
@@ -63,24 +63,24 @@ func (i *Index) update() {
 			}
 		}
 		for c < len(current) {
-			token = current[c]
-			deleted[token] = append(deleted[token], item)
+			token := current[c]
+			deleted[token] = append(deleted[token], key)
 			c++
 		}
 		for u < len(update) {
-			token = update[u]
-			inserted[token] = append(inserted[token], item)
+			token := update[u]
+			inserted[token] = append(inserted[token], key)
 			u++
 		}
 	}
-	for item, current = range i.itemTokens {
-		if _, ok = itemTokens[item]; !ok {
-			itemTokens[item] = current
+	for key, current := range i.itemTokens {
+		if _, ok := itemTokens[key]; !ok {
+			itemTokens[key] = current
 		}
 	}
-	tokenItems = make(map[int][]int)
-	for token, update = range deleted {
-		current, ok = tokenItems[token]
+	tokenItems := make(map[int][]int)
+	for token, update := range deleted {
+		current, ok := tokenItems[token]
 		if !ok {
 			current = append(current, i.tokenItems[token]...)
 		}
@@ -88,8 +88,8 @@ func (i *Index) update() {
 		current = arrays.Except(current, update)
 		tokenItems[token] = current
 	}
-	for token, update = range inserted {
-		current, ok = tokenItems[token]
+	for token, update := range inserted {
+		current, ok := tokenItems[token]
 		if !ok {
 			current = append(current, i.tokenItems[token]...)
 		}
@@ -97,8 +97,8 @@ func (i *Index) update() {
 		sort.Ints(current)
 		tokenItems[token] = current
 	}
-	for token, current = range i.tokenItems {
-		if _, ok = tokenItems[token]; !ok {
+	for token, current := range i.tokenItems {
+		if _, ok := tokenItems[token]; !ok {
 			tokenItems[token] = current
 		}
 	}
