b11b1f8d39609f2a235fed31777ba3d859137c96
FreePK <freepk@mail.ru>
2018-04-20 00:04:08 +0300

    refactor

--- inverted.go
+++ inverted.go
@@ -7,97 +7,97 @@ import (
 )
 
 type Index struct {
-	docTokens    map[int][]int
-	tokenDocs    map[int][]int
-	newDocTokens map[int][]int
+	docToks    map[int][]int
+	tokDocs    map[int][]int
+	newDocToks map[int][]int
 }
 
 func NewIndex() *Index {
 	return &Index{
-		docTokens:    make(map[int][]int),
-		tokenDocs:    make(map[int][]int),
-		newDocTokens: make(map[int][]int)}
+		docToks:    make(map[int][]int),
+		tokDocs:    make(map[int][]int),
+		newDocToks: make(map[int][]int)}
 }
 
-func (i *Index) Append(docId int, tokenIds []int) {
-	i.newDocTokens[docId] = tokenIds
+func (i *Index) Append(doc int, toks []int) {
+	i.newDocToks[doc] = toks
 }
 
-func (i *Index) Doc(docId int) []int {
-	return i.docTokens[docId]
+func (i *Index) Doc(doc int) []int {
+	return i.docToks[doc]
 }
 
-func (i *Index) Docs(tokenId int) []int {
-	return i.tokenDocs[tokenId]
+func (i *Index) Docs(tok int) []int {
+	return i.tokDocs[tok]
 }
 
-func copyNotExists(dst, src map[int][]int) {
-	for k, v := range src {
-		if _, ok := dst[k]; !ok {
-			dst[k] = v
+func copyMap(d, s map[int][]int) {
+	for k, v := range s {
+		if _, ok := d[k]; !ok {
+			d[k] = v
 		}
 	}
 }
 
 func (i *Index) Update() {
-	docTokens := i.newDocTokens
-	i.newDocTokens = make(map[int][]int)
-	deleteIds := make(map[int][]int)
-	insertIds := make(map[int][]int)
-	for docId, newTokenIds := range docTokens {
-		sort.Ints(newTokenIds)
-		newTokenIds = arrays.Distinct(newTokenIds)
-		docTokens[docId] = newTokenIds
-		oldTokenIds := i.docTokens[docId]
+	docToks := i.newDocToks
+	i.newDocToks = make(map[int][]int)
+	delTokDocs := make(map[int][]int)
+	insTokDocs := make(map[int][]int)
+	for doc, newToks := range docToks {
+		sort.Ints(newToks)
+		newToks = arrays.Distinct(newToks)
+		docToks[doc] = newToks
+		oldToks := i.docToks[doc]
 		ins := 0
 		del := 0
-		for (ins < len(newTokenIds)) && (del < len(oldTokenIds)) {
+		for (ins < len(newToks)) && (del < len(oldToks)) {
 			switch {
-			case oldTokenIds[del] < newTokenIds[ins]:
-				tokenId := oldTokenIds[del]
-				deleteIds[tokenId] = append(deleteIds[tokenId], docId)
+			case oldToks[del] < newToks[ins]:
+				tok := oldToks[del]
+				delTokDocs[tok] = append(delTokDocs[tok], doc)
 				del++
-			case oldTokenIds[del] > newTokenIds[ins]:
-				tokenId := newTokenIds[ins]
-				insertIds[tokenId] = append(insertIds[tokenId], docId)
+			case oldToks[del] > newToks[ins]:
+				tok := newToks[ins]
+				insTokDocs[tok] = append(insTokDocs[tok], doc)
 				ins++
 			default:
 				del++
 				ins++
 			}
 		}
-		for del < len(oldTokenIds) {
-			tokenId := oldTokenIds[del]
-			deleteIds[tokenId] = append(deleteIds[tokenId], docId)
+		for del < len(oldToks) {
+			tok := oldToks[del]
+			delTokDocs[tok] = append(delTokDocs[tok], doc)
 			del++
 		}
-		for ins < len(newTokenIds) {
-			tokenId := newTokenIds[ins]
-			insertIds[tokenId] = append(insertIds[tokenId], docId)
+		for ins < len(newToks) {
+			tok := newToks[ins]
+			insTokDocs[tok] = append(insTokDocs[tok], doc)
 			ins++
 		}
 	}
-	copyNotExists(docTokens, i.docTokens)
-	tokenDocs := make(map[int][]int)
-	for tokenId, docIds := range deleteIds {
-		temp, ok := tokenDocs[tokenId]
+	copyMap(docToks, i.docToks)
+	tokDocs := make(map[int][]int)
+	for tok, docs := range delTokDocs {
+		temp, ok := tokDocs[tok]
 		if !ok {
-			temp = append(temp, i.tokenDocs[tokenId]...)
+			temp = append(temp, i.tokDocs[tok]...)
 		}
-		sort.Ints(docIds)
-		temp = arrays.Except(temp, docIds)
-		tokenDocs[tokenId] = temp
+		sort.Ints(docs)
+		temp = arrays.Except(temp, docs)
+		tokDocs[tok] = temp
 	}
-	for tokenId, docIds := range insertIds {
-		temp, ok := tokenDocs[tokenId]
+	for tok, docs := range insTokDocs {
+		temp, ok := tokDocs[tok]
 		if !ok {
-			temp = append(temp, i.tokenDocs[tokenId]...)
+			temp = append(temp, i.tokDocs[tok]...)
 		}
-		temp = append(temp, docIds...)
+		temp = append(temp, docs...)
 		sort.Ints(temp)
-		tokenDocs[tokenId] = temp
+		tokDocs[tok] = temp
 	}
-	copyNotExists(tokenDocs, i.tokenDocs)
-	i.docTokens = docTokens
-	i.tokenDocs = tokenDocs
+	copyMap(tokDocs, i.tokDocs)
+	i.docToks = docToks
+	i.tokDocs = tokDocs
 }
